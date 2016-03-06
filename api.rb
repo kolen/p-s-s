60040481c1340ebaf9e6931db5cee6421461f4b9
kolen <incredible.angst@gmail.com>
2016-03-07 02:41:04 +0300

    Cookie authentication for API

--- api.rb
+++ api.rb
@@ -1,9 +1,18 @@
 require 'grape'
 
 class API < Grape::API
+  format :json
   prefix :api
 
+  before do
+    env['warden'].authenticate!
+  end
+
+  use Warden::Manager do |manager|
+    manager.failure_app = ->(_) { [401, {}, ['Not authorized']] }
+  end
+
   get :hello do
-    [{ hello: 'world' }]
+    { hello: 'world' }
   end
 end
